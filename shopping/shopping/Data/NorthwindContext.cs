dda255fad0fd6f1352c467107cbf1e8e3763a7f4
Nititorn Donsay <160242469+Nititorn3D12@users.noreply.github.com>
2024-08-21 01:04:37 +0700

    MVC

---
+++ shopping/shopping/Data/NorthwindContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shopping.Models;
+
+namespace shopping.Data
+{
+    public class NorthwindContext : DbContext
+
+    {
+        //Constructor
+        public NorthwindContext(DbContextOptions<NorthwindContext> options) : base(options) { }
+        public NorthwindContext() { }
+
+        public DbSet<Product> product { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Mapping + จัดการ relations
+            modelBuilder.Entity<Product>().ToTable("product");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string server = "localhost";
+            string database = "my_db";
+            string uid = "root";
+            string password = "12345678";
+            string connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";PORT=3306;SslMode=Required;";
+            optionsBuilder.UseMySQL(connectionString);
+        }
+
+    }
+}
---
+++ shopping/shopping/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace shopping.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+        public string description { get; set; }
+        public int stock { get; set; }
+
+    }
+}
